f15d68d8baaff37ea5f9c225cc58ac996dbfc24b
waitwind <jinpujun@gmail.com>
2014-12-06 15:28:40 +0800

    它山石 index page 80%

---
+++ clover/treemap-dash-json.js
@@ -0,0 +1,301 @@
+processTreeMapDashJson (  {"id":"Clover database 星期六 十一月 29 2014 13:57:37 CST0","name":"","data":{
+    "$area":15770.0,"$color":89.96829,"title":
+    " 15770 Elements, 90% Coverage"},"children":[{"id":
+      "com.jinhe.tss.cache.extension.threadpool156","name":
+      "com.jinhe.tss.cache.extension.threadpool","data":{"$area":78.0,
+        "$color":87.17949,"title":
+        "com.jinhe.tss.cache.extension.threadpool 78 Elements, 87.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cache.extension.workqueue234",
+      "name":"com.jinhe.tss.cache.extension.workqueue","data":{"$area":
+        57.0,"$color":91.228065,"title":
+        "com.jinhe.tss.cache.extension.workqueue 57 Elements, 91.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cache.extension291","name":
+      "com.jinhe.tss.cache.extension","data":{"$area":145.0,"$color":
+        84.82758,"title":
+        "com.jinhe.tss.cache.extension 145 Elements, 84.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cache436","name":
+      "com.jinhe.tss.cache","data":{"$area":777.0,"$color":89.70399,"title":
+        "com.jinhe.tss.cache 777 Elements, 89.7% Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.util.proxy1549","name":
+      "com.jinhe.tss.util.proxy","data":{"$area":48.0,"$color":97.91667,
+        "title":"com.jinhe.tss.util.proxy 48 Elements, 97.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.util1644","name":
+      "com.jinhe.tss.util","data":{"$area":1179.0,"$color":91.43342,"title":
+        "com.jinhe.tss.util 1179 Elements, 91.4% Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.framework.exception.convert3178","name":
+      "com.jinhe.tss.framework.exception.convert","data":{"$area":50.0,
+        "$color":90.0,"title":
+        "com.jinhe.tss.framework.exception.convert 50 Elements, 90% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.exception3236","name":
+      "com.jinhe.tss.framework.exception","data":{"$area":101.0,"$color":
+        82.178215,"title":
+        "com.jinhe.tss.framework.exception 101 Elements, 82.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.test3382","name":
+      "com.jinhe.tss.framework.test","data":{"$area":125.0,"$color":90.4,
+        "title":
+        "com.jinhe.tss.framework.test 125 Elements, 90.4% Coverage"},
+      "children":[]},{"id":
+      "com.jinhe.tss.framework.persistence.pagequery3519","name":
+      "com.jinhe.tss.framework.persistence.pagequery","data":{"$area":
+        210.0,"$color":93.809525,"title":
+        "com.jinhe.tss.framework.persistence.pagequery 210 Elements, 93.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.mvc3832","name":
+      "com.jinhe.tss.framework.web.mvc","data":{"$area":78.0,"$color":
+        88.46153,"title":
+        "com.jinhe.tss.framework.web.mvc 78 Elements, 88.5% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.dispaly.grid3910",
+      "name":"com.jinhe.tss.framework.web.dispaly.grid","data":{"$area":
+        205.0,"$color":93.65854,"title":
+        "com.jinhe.tss.framework.web.dispaly.grid 205 Elements, 93.7% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.filter4115","name":
+      "com.jinhe.tss.framework.web.filter","data":{"$area":367.0,"$color":
+        83.378746,"title":
+        "com.jinhe.tss.framework.web.filter 367 Elements, 83.4% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.dispaly4512","name":
+      "com.jinhe.tss.framework.web.dispaly","data":{"$area":120.0,"$color":
+        93.333336,"title":
+        "com.jinhe.tss.framework.web.dispaly 120 Elements, 93.3% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.dispaly.xform4632",
+      "name":"com.jinhe.tss.framework.web.dispaly.xform","data":{"$area":
+        54.0,"$color":87.03704,"title":
+        "com.jinhe.tss.framework.web.dispaly.xform 54 Elements, 87% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.persistence4686","name":
+      "com.jinhe.tss.framework.persistence","data":{"$area":354.0,"$color":
+        92.93785,"title":
+        "com.jinhe.tss.framework.persistence 354 Elements, 92.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.sso.appserver5062",
+      "name":"com.jinhe.tss.framework.sso.appserver","data":{"$area":112.0,
+        "$color":96.42857,"title":
+        "com.jinhe.tss.framework.sso.appserver 112 Elements, 96.4% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.sso5251","name":
+      "com.jinhe.tss.framework.sso","data":{"$area":177.0,"$color":88.1356,
+        "title":"com.jinhe.tss.framework.sso 177 Elements, 88.1% Coverage"},
+      "children":[]},{"id":
+      "com.jinhe.tss.framework.component.progress5617","name":
+      "com.jinhe.tss.framework.component.progress","data":{"$area":84.0,
+        "$color":90.47619,"title":
+        "com.jinhe.tss.framework.component.progress 84 Elements, 90.5% Coverage"},
+      "children":[]},{"id":
+      "com.jinhe.tss.framework.persistence.entityaop5765","name":
+      "com.jinhe.tss.framework.persistence.entityaop","data":{"$area":
+        169.0,"$color":94.67456,"title":
+        "com.jinhe.tss.framework.persistence.entityaop 169 Elements, 94.7% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.license5993","name":
+      "com.jinhe.tss.framework.license","data":{"$area":143.0,"$color":
+        90.909096,"title":
+        "com.jinhe.tss.framework.license 143 Elements, 90.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework6150","name":
+      "com.jinhe.tss.framework","data":{"$area":63.0,"$color":90.47619,
+        "title":"com.jinhe.tss.framework 63 Elements, 90.5% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.sso.context6294","name":
+      "com.jinhe.tss.framework.sso.context","data":{"$area":170.0,"$color":
+        95.88235,"title":
+        "com.jinhe.tss.framework.sso.context 170 Elements, 95.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.sso.online6565","name":
+      "com.jinhe.tss.framework.sso.online","data":{"$area":101.0,"$color":
+        84.15842,"title":
+        "com.jinhe.tss.framework.sso.online 101 Elements, 84.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web6698","name":
+      "com.jinhe.tss.framework.web","data":{"$area":6.0,"$color":0.0,"title":
+        "com.jinhe.tss.framework.web 6 Elements, 0% Coverage"},"children":[]},
+    {"id":"com.jinhe.tss.framework.web.dispaly.tree6704","name":
+      "com.jinhe.tss.framework.web.dispaly.tree","data":{"$area":200.0,
+        "$color":91.0,"title":
+        "com.jinhe.tss.framework.web.dispaly.tree 200 Elements, 91% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.component.cache6904",
+      "name":"com.jinhe.tss.framework.component.cache","data":{"$area":
+        224.0,"$color":95.98214,"title":
+        "com.jinhe.tss.framework.component.cache 224 Elements, 96% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.sso.identifier7171",
+      "name":"com.jinhe.tss.framework.sso.identifier","data":{"$area":33.0,
+        "$color":93.93939,"title":
+        "com.jinhe.tss.framework.sso.identifier 33 Elements, 93.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.component.timer7204",
+      "name":"com.jinhe.tss.framework.component.timer","data":{"$area":
+        96.0,"$color":86.45833,"title":
+        "com.jinhe.tss.framework.component.timer 96 Elements, 86.5% Coverage"},
+      "children":[]},{"id":
+      "com.jinhe.tss.framework.web.dispaly.xmlhttp7330","name":
+      "com.jinhe.tss.framework.web.dispaly.xmlhttp","data":{"$area":61.0,
+        "$color":95.08197,"title":
+        "com.jinhe.tss.framework.web.dispaly.xmlhttp 61 Elements, 95.1% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.servlet7391","name":
+      "com.jinhe.tss.framework.web.servlet","data":{"$area":111.0,"$color":
+        76.57658,"title":
+        "com.jinhe.tss.framework.web.servlet 111 Elements, 76.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.component.param7542",
+      "name":"com.jinhe.tss.framework.component.param","data":{"$area":
+        421.0,"$color":95.96199,"title":
+        "com.jinhe.tss.framework.component.param 421 Elements, 96% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.component.log8110",
+      "name":"com.jinhe.tss.framework.component.log","data":{"$area":212.0,
+        "$color":98.58491,"title":
+        "com.jinhe.tss.framework.component.log 212 Elements, 98.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.listener8397","name":
+      "com.jinhe.tss.framework.web.listener","data":{"$area":19.0,"$color":
+        89.47369,"title":
+        "com.jinhe.tss.framework.web.listener 19 Elements, 89.5% Coverage"},
+      "children":[]},{"id":
+      "com.jinhe.tss.framework.persistence.connpool8416","name":
+      "com.jinhe.tss.framework.persistence.connpool","data":{"$area":109.0,
+        "$color":78.899086,"title":
+        "com.jinhe.tss.framework.persistence.connpool 109 Elements, 78.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.rmi8536","name":
+      "com.jinhe.tss.framework.web.rmi","data":{"$area":193.0,"$color":
+        89.63731,"title":
+        "com.jinhe.tss.framework.web.rmi 193 Elements, 89.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.framework.web.wrapper8758","name":
+      "com.jinhe.tss.framework.web.wrapper","data":{"$area":103.0,"$color":
+        91.26214,"title":
+        "com.jinhe.tss.framework.web.wrapper 103 Elements, 91.3% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.entity9526","name":
+      "com.jinhe.tss.cms.entity","data":{"$area":267.0,"$color":91.7603,
+        "title":"com.jinhe.tss.cms.entity 267 Elements, 91.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.lucene9793","name":
+      "com.jinhe.tss.cms.lucene","data":{"$area":152.0,"$color":86.84211,
+        "title":"com.jinhe.tss.cms.lucene 152 Elements, 86.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.lucene.executor10046","name":
+      "com.jinhe.tss.cms.lucene.executor","data":{"$area":87.0,"$color":
+        88.505745,"title":
+        "com.jinhe.tss.cms.lucene.executor 87 Elements, 88.5% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.entity.permission10133","name":
+      "com.jinhe.tss.cms.entity.permission","data":{"$area":2.0,"$color":
+        0.0,"title":
+        "com.jinhe.tss.cms.entity.permission 2 Elements, 0% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.job10135","name":
+      "com.jinhe.tss.cms.job","data":{"$area":174.0,"$color":94.25287,
+        "title":"com.jinhe.tss.cms.job 174 Elements, 94.3% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms.helper10425","name":
+      "com.jinhe.tss.cms.helper","data":{"$area":225.0,"$color":92.0,"title":
+        "com.jinhe.tss.cms.helper 225 Elements, 92% Coverage"},"children":[]},
+    {"id":"com.jinhe.tss.cms.service10689","name":
+      "com.jinhe.tss.cms.service","data":{"$area":518.0,"$color":88.416985,
+        "title":"com.jinhe.tss.cms.service 518 Elements, 88.4% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.cms11207","name":
+      "com.jinhe.tss.cms","data":{"$area":119.0,"$color":75.63026,"title":
+        "com.jinhe.tss.cms 119 Elements, 75.6% Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.cms.dao11487","name":"com.jinhe.tss.cms.dao",
+      "data":{"$area":161.0,"$color":91.92547,"title":
+        "com.jinhe.tss.cms.dao 161 Elements, 91.9% Coverage"},"children":[]},
+    {"id":"com.jinhe.tss.cms.action11648","name":
+      "com.jinhe.tss.cms.action","data":{"$area":185.0,"$color":92.97297,
+        "title":"com.jinhe.tss.cms.action 185 Elements, 93% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.helper.dto12024","name":
+      "com.jinhe.tss.um.helper.dto","data":{"$area":167.0,"$color":
+        85.029945,"title":
+        "com.jinhe.tss.um.helper.dto 167 Elements, 85% Coverage"},"children":
+      []},{"id":"com.jinhe.tss.um.permission.filter12191","name":
+      "com.jinhe.tss.um.permission.filter","data":{"$area":80.0,"$color":
+        82.5,"title":
+        "com.jinhe.tss.um.permission.filter 80 Elements, 82.5% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.search12271","name":
+      "com.jinhe.tss.um.search","data":{"$area":84.0,"$color":92.85714,
+        "title":"com.jinhe.tss.um.search 84 Elements, 92.9% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.entity.permission12395","name":
+      "com.jinhe.tss.um.entity.permission","data":{"$area":46.0,"$color":
+        39.130436,"title":
+        "com.jinhe.tss.um.entity.permission 46 Elements, 39.1% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.dao.impl12441","name":
+      "com.jinhe.tss.um.dao.impl","data":{"$area":295.0,"$color":93.559326,
+        "title":"com.jinhe.tss.um.dao.impl 295 Elements, 93.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.service.impl12736","name":
+      "com.jinhe.tss.um.service.impl","data":{"$area":816.0,"$color":
+        92.76961,"title":
+        "com.jinhe.tss.um.service.impl 816 Elements, 92.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.permission13552","name":
+      "com.jinhe.tss.um.permission","data":{"$area":654.0,"$color":
+        86.23853,"title":
+        "com.jinhe.tss.um.permission 654 Elements, 86.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.syncdata14208","name":
+      "com.jinhe.tss.um.syncdata","data":{"$area":185.0,"$color":87.567566,
+        "title":"com.jinhe.tss.um.syncdata 185 Elements, 87.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.dao14573","name":
+      "com.jinhe.tss.um.dao","data":{"$area":0.0,"$color":-100.0,"title":
+        "com.jinhe.tss.um.dao 0 Elements,  -  Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.um.sso14573","name":"com.jinhe.tss.um.sso","data":{
+        "$area":37.0,"$color":72.97297,"title":
+        "com.jinhe.tss.um.sso 37 Elements, 73% Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.um.sso.othersystem14652","name":
+      "com.jinhe.tss.um.sso.othersystem","data":{"$area":68.0,"$color":
+        67.64706,"title":
+        "com.jinhe.tss.um.sso.othersystem 68 Elements, 67.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.action14748","name":
+      "com.jinhe.tss.um.action","data":{"$area":477.0,"$color":94.75891,
+        "title":"com.jinhe.tss.um.action 477 Elements, 94.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.sso.online15225","name":
+      "com.jinhe.tss.um.sso.online","data":{"$area":55.0,"$color":87.27273,
+        "title":"com.jinhe.tss.um.sso.online 55 Elements, 87.3% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.syncdata.dao15317","name":
+      "com.jinhe.tss.um.syncdata.dao","data":{"$area":239.0,"$color":
+        82.845184,"title":
+        "com.jinhe.tss.um.syncdata.dao 239 Elements, 82.8% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.servlet15556","name":
+      "com.jinhe.tss.um.servlet","data":{"$area":107.0,"$color":94.392525,
+        "title":"com.jinhe.tss.um.servlet 107 Elements, 94.4% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um15748","name":
+      "com.jinhe.tss.um","data":{"$area":0.0,"$color":-100.0,"title":
+        "com.jinhe.tss.um 0 Elements,  -  Coverage"},"children":[]},{"id":
+      "com.jinhe.tss.um.permission.dispaly15781","name":
+      "com.jinhe.tss.um.permission.dispaly","data":{"$area":74.0,"$color":
+        98.64865,"title":
+        "com.jinhe.tss.um.permission.dispaly 74 Elements, 98.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.um.service15855","name":
+      "com.jinhe.tss.um.service","data":{"$area":0.0,"$color":-100.0,"title":
+        "com.jinhe.tss.um.service 0 Elements,  -  Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.um.entity15855","name":"com.jinhe.tss.um.entity",
+      "data":{"$area":537.0,"$color":95.71695,"title":
+        "com.jinhe.tss.um.entity 537 Elements, 95.7% Coverage"},"children":[]},
+    {"id":"com.jinhe.tss.um.helper16392","name":"com.jinhe.tss.um.helper",
+      "data":{"$area":193.0,"$color":92.227974,"title":
+        "com.jinhe.tss.um.helper 193 Elements, 92.2% Coverage"},"children":[]},
+    {"id":"com.jinhe.tss.portal.service.impl17371","name":
+      "com.jinhe.tss.portal.service.impl","data":{"$area":420.0,"$color":
+        93.09524,"title":
+        "com.jinhe.tss.portal.service.impl 420 Elements, 93.1% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.action17791","name":
+      "com.jinhe.tss.portal.action","data":{"$area":594.0,"$color":
+        92.08754,"title":
+        "com.jinhe.tss.portal.action 594 Elements, 92.1% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.entity.permission18385",
+      "name":"com.jinhe.tss.portal.entity.permission","data":{"$area":4.0,
+        "$color":0.0,"title":
+        "com.jinhe.tss.portal.entity.permission 4 Elements, 0% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.dao.impl18389","name":
+      "com.jinhe.tss.portal.dao.impl","data":{"$area":112.0,"$color":
+        86.60714,"title":
+        "com.jinhe.tss.portal.dao.impl 112 Elements, 86.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.helper18501","name":
+      "com.jinhe.tss.portal.helper","data":{"$area":221.0,"$color":
+        85.52036,"title":
+        "com.jinhe.tss.portal.helper 221 Elements, 85.5% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal18722","name":
+      "com.jinhe.tss.portal","data":{"$area":57.0,"$color":0.0,"title":
+        "com.jinhe.tss.portal 57 Elements, 0% Coverage"},"children":[]},{
+      "id":"com.jinhe.tss.portal.engine.model18915","name":
+      "com.jinhe.tss.portal.engine.model","data":{"$area":270.0,"$color":
+        92.59259,"title":
+        "com.jinhe.tss.portal.engine.model 270 Elements, 92.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.permission19185","name":
+      "com.jinhe.tss.portal.permission","data":{"$area":77.0,"$color":
+        79.22078,"title":
+        "com.jinhe.tss.portal.permission 77 Elements, 79.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.engine19262","name":
+      "com.jinhe.tss.portal.engine","data":{"$area":595.0,"$color":
+        86.55463,"title":
+        "com.jinhe.tss.portal.engine 595 Elements, 86.6% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.entity19939","name":
+      "com.jinhe.tss.portal.entity","data":{"$area":573.0,"$color":
+        93.19372,"title":
+        "com.jinhe.tss.portal.entity 573 Elements, 93.2% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.service20520","name":
+      "com.jinhe.tss.portal.service","data":{"$area":0.0,"$color":-100.0,
+        "title":"com.jinhe.tss.portal.service 0 Elements,  -  Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.engine.macrocode20520",
+      "name":"com.jinhe.tss.portal.engine.macrocode","data":{"$area":88.0,
+        "$color":92.045456,"title":
+        "com.jinhe.tss.portal.engine.macrocode 88 Elements, 92% Coverage"},
+      "children":[]},{"id":"com.jinhe.tss.portal.dao20608","name":
+      "com.jinhe.tss.portal.dao","data":{"$area":0.0,"$color":-100.0,"title":
+        "com.jinhe.tss.portal.dao 0 Elements,  -  Coverage"},"children":[]}]}
+
+ ); 
